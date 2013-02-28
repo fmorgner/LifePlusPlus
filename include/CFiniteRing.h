750398be863f7c2c19d2071b5cf93f911f5d5302
Felix Morgner <felix.morgner@gmail.com>
2013-02-28 08:33:17 -0800

    added correct license header

--- include/CFiniteRing.h
+++ include/CFiniteRing.h
@@ -1,10 +1,32 @@
-//
-//  CFixedRing.h
-//  Life++
-//
-//  Created by Felix Morgner on 27.02.13.
-//  Copyright (c) 2013 Felix Morgner. All rights reserved.
-//
+/*
+ *
+ * FiniteRing.h
+ * Part of Life++ - Yet another implementation of the Game of Life in C++
+ * -------------------------------------------------------------------------
+ * begin                 : 2013-02-27
+ * copyright             : Copyright (C) 2013 by Felix Morgner
+ * email                 : felix.morgner@gmail.com
+ * =========================================================================
+ *                                                                         |
+ *   This program is free software; you can redistribute it and/or modify  |
+ *   it under the terms of the GNU General Public License as published by  |
+ *   the Free Software Foundation; either version 2 of the License, or     |
+ *   (at your option) any later version.                                   |
+ *                                                                         |
+ *   This program is distributed in the hope that it will be useful,       |
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        |
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         |
+ *   GNU General Public License for more details.                          |
+ *                                                                         |
+ *   You should have received a copy of the GNU General Public License     |
+ *   along with this program; if not, write to the                         |
+ *                                                                         |
+ *   Free Software Foundation, Inc.,                                       |
+ *   59 Temple Place Suite 330,                                            |
+ *   Boston, MA  02111-1307, USA.                                          |
+ * =========================================================================
+ *
+ */
 
 #ifndef Life___CFiniteRing_h
 #define Life___CFiniteRing_h
