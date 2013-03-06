26bf07f2518f08c223498cd552a372d5657a4d2d
Felix Morgner <felix.morgner@gmail.com>
2013-03-06 13:29:07 -0800

    updates

--- release-build.sh
+++ release-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-clang -O3 -std=c++11 -stdlib=libc++ -lncurses -lc++abi -lpthread -lc++ src/CCell.cpp src/CWorld.cpp src/main.cpp -Iinclude -o Life++
+mkdir -p build/Release/
 
+clang -O3 -g0 -std=c++11 -stdlib=libc++ -lncurses -lc++abi -lpthread -lc++ src/CCell.cpp src/CWorld.cpp src/main.cpp -Iinclude -o build/Release/Life++
